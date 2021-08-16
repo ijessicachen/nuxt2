473a6299300f46c69463300460e6e84bd6912b21
Jessica Chen <jessicachen@Jessicas-MacBook-Air-151.local>
2021-08-15 22:49:20 -0400

    website yay

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1> HELLO WORLD 2 </h1>
+        <h4> ____________________ </h4>
+        <ul>
+            <li> aujourd'hui </li>
+            <li> l'arbre est grand </li>
+        </ul>
+    </div>
+</template>
