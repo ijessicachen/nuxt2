b8e15d6a9d979dba4873f92c6ad4fa769b95fa28
= <=>
2021-08-25 19:29:43 -0400

    start playing around with vuetify buttons component

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template lang="pug">
-    div
-        h1 HELLO WORLD 2 
-        h4 ____________________ 
+    v-app
+        h1  HELLO WORLD 2 
+        h4  ____________________ 
         ul
-            li aujourd'hui 
-            li l'arbre est grand 
+            li  aujourd'hui 
+            li  l'arbre est grand 
+        
+        //try the button component.
+        v-btn(
+          depressed
+          color = "primary"
+        ) Normal
 </template>
