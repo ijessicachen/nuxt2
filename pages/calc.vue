714bf8e05605150f39e52613defede38d2bcddc3
= <=>
2021-08-25 18:51:30 -0400

    add new page. calculator.

---
+++ pages/calc.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+
+    table
+        tr
+          th(colspan = "4") 0000000000
+        tr
+          td 7
+          td 8
+          td 9
+          td x
+        tr
+          td 4
+          td 5
+          td 6
+          td -
+        tr
+          td 1
+          td 2
+          td 3
+          td +
+        tr
+          td 1
+          td 1
+          td 1
+          td 1
+          
+</template>
