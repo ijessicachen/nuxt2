5dd4e36c587da05360bf80d4f6c0fe550efd2aa4
= <=>
2021-08-26 19:50:37 -0400

    started making the visual aspect of buttons for the basic calculator

--- pages/calc.vue
+++ pages/calc.vue
@@ -1,27 +1,115 @@
 <template lang="pug">
 
-    table
-        tr
-          th(colspan = "4") 0000000000
-        tr
-          td 7
-          td 8
-          td 9
-          td x
-        tr
-          td 4
-          td 5
-          td 6
-          td -
-        tr
-          td 1
-          td 2
-          td 3
-          td +
-        tr
-          td 1
-          td 1
-          td 1
-          td 1
+    v-app
+        table
+            tr
+              th(colspan = "4") 0000000000
+            tr
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon(
+                        color = "grey lighten-5"
+                    ) mdi-numeric-7-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon(
+                        color = "grey lighten-5"
+                    ) mdi-numeric-8-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-close
+            tr
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-4-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-5-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-6-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-minus
+            tr
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-1-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-2-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-3-circle
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-plus
+            tr
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "deep-purple darken-4"
+                ) 
+                    v-icon mdi-numeric-0-circle
+              td 
+              td 
+              td 
+                v-btn(
+                 small
+                 fab
+                 color = "red lighten-1"
+                ) 
+                    v-icon mdi-slash-forward
           
 </template>
