521f73cc8bfef815957ede0f2e8de56f7044a6dd
Jessica Chen <jessica.chen@leocorn.com>
2022-10-02 17:35:52 -0400

    squaring works, also added non-functioning buttons for other operations + 2nd function.

--- pages/calc.vue
+++ pages/calc.vue
@@ -13,10 +13,11 @@ v-app
     ul
         li Goal is a functioning calculator with at least basic 4 ops 
         li 4 basic ops work, adding brackets, more complicated operations, and more!
-    // calculator graphics
+
+    // CALCULATOR GRAPHICS
     v-card.my-8(
         class = "mx-auto"
-        max-width = "500"
+        max-width = "624"
         outlined
     )
         table
@@ -24,7 +25,7 @@ v-app
             //blocking typing in it since I don't want to deal with that
             //yet so I'll just remove the option
             tr
-              th(colspan = "4")
+              th(colspan = "5")
                 v-text-field(
                     filled
                     readonly 
@@ -33,7 +34,7 @@ v-app
                 ) 
             // clear button
             tr
-              th(colspan = "4")
+              th(colspan = "5")
                  v-btn(
                    medium
                    block 
@@ -43,6 +44,14 @@ v-app
                  ) clear
             // number rows
             tr
+              td
+                 v-btn(
+                     large
+                     fab
+                     color = "green accent-4"
+                     @click = "numberClick('sqr')"
+                 )
+                     v-icon mdi-format-superscript
               td( 
                 v-for = "n in [7, 8, 9]"
                 :key = "n"
@@ -65,6 +74,14 @@ v-app
                 ) 
                     v-icon mdi-close
             tr
+              td
+                 v-btn(
+                     large
+                     fab
+                     color = "green accent-4"
+                     @click = "numberClick('sqrt')"
+                 )
+                     v-icon mdi-square-root
               td(
                   v-for = "n in [4, 5, 6]"
                   :key = "n"
@@ -87,6 +104,13 @@ v-app
                 ) 
                     v-icon mdi-minus
             tr
+              td
+                 v-btn(
+                     large
+                     fab
+                     color = "green accent-4"
+                 )
+                     v-icon mdi-exclamation
               td(
                   v-for = "n in [1, 2, 3]"
                   :key = "n"
@@ -109,6 +133,14 @@ v-app
                 ) 
                     v-icon mdi-plus
             tr
+              td
+                v-btn(
+                  large
+                  color = "green accent-4"
+                  fab
+                  @click = "numberClick('f')"
+                )
+                    v-icon mdi-plus-minus-variant 
               td 
                 v-btn(
                  large
@@ -149,15 +181,13 @@ v-app
             // last row (+- and =)
             tr
               td
-                v-btn(
-                  large
-                  color = "green accent-4"
-                  fab
-                  @click = "numberClick('f')"
-                )
-                    v-icon mdi-plus-minus-variant 
-              
-              th(colspan = "4")
+                 v-btn(
+                     large
+                     fab
+                     color = "blue lighten-1"
+                 )
+                     v-icon mdi-code-parentheses
+              th(colspan = "3")
                 v-btn(
                  large 
                  color = "red lighten-1"
@@ -167,16 +197,15 @@ v-app
                     v-icon(
                        large
                     ) mdi-equal
-            //maybe make a new number pad for further operations
-            //td
-              //  v-btn(
-              //    large
-              //    fab
-              //    color = "red lighten-1"
-              //  )
-              //      v-icon mdi-exponent
-              //td
-          
+              td
+                 // FIGURE OUT HOW YOU CAN CHANGE ELEMENTS OF A BUTTON
+                 v-btn( 
+                     large
+                     fab
+                     outlined
+                     color = "black"
+                 )
+                     v-icon mdi-arrow-up-bold-circle
 </template>
 
 <script>
@@ -185,6 +214,7 @@ export default {
    data: function(){
        return{
            r1: ["red lighten-1", "red lighten-1", "red lighten-1", "red lighten-1"], // google operation thing
+           b1: ["blue lighten-1", "blue lighten-1", "blue lighten-1", "blue lighten-1"],
            result: "0",
            first: null,
            hold: [], // pretty sure whole equation will end up here
@@ -206,14 +236,6 @@ export default {
        //number button clicking
        numberClick: function(n) {
 
-           // idk what this is
-           /*
-           if( this.first == this.result){
-               this.result == null;
-           }
-           */
-
-           
            // del or backspace
            if( n === "<-"){
                if (this.places === 0){
@@ -226,6 +248,10 @@ export default {
            // +/-
            else if(n === "f"){
                this.result *= -1;
+           }
+           // ^2
+           else if (n === "sqr"){
+               this.result *= this.result;
            }
            // normal numbers
            else{
@@ -246,32 +272,12 @@ export default {
                }
            }
 
-           /*
-              I think right now I'm trying to create a long string to
-              account for brackets but I think I should be able to calculate
-              it until there is a bracket
-           */
-           // the structure is supposed to account for brackets rn but there
-           // aren't actually any brackets implemented
-           // DO NOT USE BRACKETS YET WHEN TESTING
-           /*
-              Okay so FORGET about the string (for now?) and just 
-              calculate as you go.
-              Yes this is basically reverting to the first version
-              of the calculator but shhh
-           */
-           /*
-           else{
-               this.first = this.result;
-               // acdtauly you may eventually need ot also put an operations clause here
-           }
-           */
-
        },
+
+
        //operations 
        // redoing them so the operations can stack into an array
        // WAIT YOU CAN PROBABLY JUST MAKE RECURSIVE FOR BRACKETS SO SCREW THE ARRAY (for now?)
-       // yet another redo 
        operations: function(m) {
            this.r1 = ["red lighten-1", "red lighten-1", "red lighten-1", "red lighten-1"]
 
@@ -354,31 +360,6 @@ export default {
                this.op = "/";
                this.r1.splice(3, 1, "pink lighten-2");
            }
-
-           // do something here to calculate the previous 
-           // components of hold so like ideally there should be
-           // max two elements in hold
-           /*
-           let check = hold;
-           while (true){
-               if (this.check.length === 2 && (this.check.at(1).isArray() && this.check.at(1).at(length-1).charAt(length-1) != ')')){
-                   this.check = this.check.at(1);
-               } else if (this.check.length === 2){
-                   var one = this.check.at(0).substring(0, this.check.at(0).length-2);
-                   var op = this.check.at(0).charAt(this.check.at(0).length-1);
-                   var o = this.op.charCodeAt(0);
-                   switch(o){
-                       case 43:
-                           break;
-                       case 45:
-                           break;
-                       case 47:
-                           break;
-                       default: //multiplication
-                   };
-               }
-           }
-           */
 
        }
    }
