831e170be86228e864b44edac752f3e644fc1591
Jessica Chen <jessica.chen@leocorn.com>
2022-11-14 03:14:36 -0500

    bro I tried to work with theme, realized that was way over my head, then tried to do the basic button things for it. That didn't work. Honestly just trying to get used to this language again, again.

--- components/Header.vue
+++ components/Header.vue
@@ -17,10 +17,54 @@ div
        v-btn(
            icon
            small
+           v-bind:color = "modes.at(state)"
            outlined
+           @click = "ahmyeyes(\"mode\")"
        ).mx-2
+           // it would be cool to make the icon change too but idk if
+           // I have that skill yet
            v-icon(
-               color = "deep-purple darken-3"
            ) {{ "mdi-moon-waning-crescent" }}
 
 </template>
+<script>
+
+export default {
+    // variables
+    data: function(){
+        return{
+            modes: ["deep-purple darken-3", "yellow accent-1"],
+            buttons: ["mdi-moon-waning-crescent", "mdi-white-balance-sunny"],
+            state: 0
+        };
+    },
+
+    computed:{
+    },
+
+    methods:{
+        ahmyeyes: function(b) {
+            if (state === 0){
+                state = 1;
+            } else{
+                state = 0;
+            }
+        }
+    }
+}
+
+// bro I'll deal with theme later, I have
+// code needed but I have no idea where 
+// to use it
+/*
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+      theme: { dark: true },
+})
+*/
+
+</script>
