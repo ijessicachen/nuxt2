90b2ffc95389f33a668724778d836553a00e97c1
Jessica Chen <jessica.chen@leocorn.com>
2023-01-15 15:13:21 -0500

    buttons are actually better now, no weird white space, added margin between first button and intro text.

--- pages/index.vue
+++ pages/index.vue
@@ -7,49 +7,60 @@
             li None are really "complete", but those tagged with (IN PROGRESS) do not yet function close enough to my actual goal for them.
         
         //buttons to other pages
-        v-btn.mt-8.white--text(
-          block
-          color = "indigo accent-2"
-          to = "/calc"
-        ) calculator (in progress) 
-        v-btn.white--text(
-          block
-          color = "blue accent-2"
-          to = "/tetris"
-        ) tetris (in progress)
-        v-btn.white--text(
-          color = "blue"
-          block
-          to = "/mines"
-        ) minesweeper
-        v-btn.white--text(
-          block
-          color = "cyan"
-          to = "/snake"
-        ) snake
-         //dunno why there's that huge white space below the buttons
-        v-row(
+        //  • now they're all in rows to make the height of each section equal
+        v-row( 
             no-gutters
-        )
-            v-col(
+            class = "mt-6"
             )
+            v-btn.white--text(
+                  height = "100%"
+                  block
+                  color = "indigo accent-2"
+                  to = "/calc"
+            ) calculator (in progress) 
+        v-row( no-gutters )
+            v-btn.white--text(
+                  height = "100%"
+                  block
+                  color = "blue accent-2"
+                  to = "/tetris"
+            ) tetris (in progress)
+        v-row( no-gutters )
+            v-btn.white--text(
+                  height = "100%"
+                  color = "blue"
+                  block
+                  to = "/mines"
+            ) minesweeper
+        v-row( no-gutters )
+            v-btn.white--text(
+                 height = "100%"
+                 block
+                 color = "cyan"
+                 to = "/snake"
+            ) snake
+        v-row( no-gutters )
+            v-col()
                v-btn.white--text(
+                   height = "100%"
                    block
                    color = "teal accent-4"
                    href = "/legacy/IT2_Stem_Club/Smoke_Detector.html"
                ) fire alarm website
-            v-col(
-            )
+            v-col()
                v-btn.white--text(
+                   height = "100%"
                    block
                    color = "teal accent-4"
                    to = "/falarm2"
                ) fire alarm website improved (in progress)
-        v-btn.white--text(
-            block
-            color = "black"
-            to = "/test"
-        ) new test home page (in progress)
+        v-row( no-gutters )
+            v-btn.white--text(
+                height = "100%"
+                block
+                color = "black"
+                to = "/test"
+            ) new test home page (in progress)
 </template>
 <script>
 export default{
