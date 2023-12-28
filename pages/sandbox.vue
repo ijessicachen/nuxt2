001f9589af39be5a898e075ebc53e775cbaed47a
Jessica Chen <jessica.chen@leocorn.com>
2023-12-27 22:42:37 -0500

    added some stuff to the sandbox (finally).

--- pages/sandbox.vue
+++ pages/sandbox.vue
@@ -29,7 +29,7 @@
                       disabled
                       tile
                       ) four
-              v-col
+              v-col(cols = "4")
                   v-card(
                       outlined
                       ) 
@@ -46,6 +46,9 @@
                               class="transition-fast-in-fast-out v-card--reveal"
                               style="height: 100%;"
                               ) 5.5 :O
+                              v-img(
+                                  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                                  )
                               v-card-actions
                                   v-btn(
                                       text
@@ -60,49 +63,85 @@
                       loading
                       ) six
           v-row
-              v-col
+              // SEVEN: image
+              v-col(cols = "4")
                   v-card(
                       class = "pa-0"
                       ) 
                       v-img(
                           cover
-                          height = "100"
-                          width = "360"
+                          inline
                           src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
                           )
                       v-card-subtitle seven
+              // EIGHT: fancy card text and background image
               v-col
                   v-card(
                       class = "pa-2"
+                      image = "https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+                      title = "test"
                       )
                       v-card-text eight
                       v-divider
                       v-card-text 8
-                      //v-divider( vertical )
-                      //v-card-text XIII
+                      v-divider( vertical )
+                      v-card-text XIII
+              // NINE: dialog
               v-col
                   v-card(
-                      outlined
-                      tile
-                      )
+                      class = "pa-2"
+                      title = "NINE"
+                      ) 
+                      v-card-actions
+                          v-btn(
+                              color = "purple"
+                              @click = "dialog=!dialog"
+                              text
+                              ) nine
                       v-dialog(
-                          v-model="dialog"
+                          v-if="dialog"
+                          width = "500"
                           )
-                          template( v-slot:activator="{ on, attrs}" )
+                          template(
+                              v-slot:activator="{ props }"
+                              )
                               v-btn(
-                                  outlined
-                                  color = "red"
-                                  ) nine
-                          v-card pop-up!
-                              v-card-actions
-                                  v-btn(
-                                      color = "primary"
-                                      text
-                                      @click="dialog = false"
-                                      ) go back
+                                  v-bind="props"
+                                  variant="outlined, tonal"
+                                  @click="isActive=true"
+                                  ) open dialog
+                          template(
+                              v-slot:default="{ isActive }"
+                              )
+                              v-card( title = "Dialog")
+                                  v-card-text() This is a dialog!
+                                  v-card-actions()
+                                      v-spacer()
+                                      v-btn(
+                                          text="Close Dialog"
+                                          @click="isActive.value = false"
+                                          )
 
+
+       // TEN: snackbar and tooltip
        div( class = "mt-4" )
-           v-btn start
+           // can't close it right now
+           //v-btn(
+           //    @click="snackbar=true"
+           //    class="mx-2"
+           //    ) open snackbar
+           //    v-snackbar(
+           //        v-model="snackbar"
+           //        ) {{ text }}
+           //        template(
+           //            v-slot:actions
+           //            )
+           //            v-btn(
+           //                color="pink"
+           //                variant="text"
+           //                @click="snackbar=false"
+           //                )
+           v-btn
                v-tooltip(
                    activator = "parent"
                    location="start"
@@ -112,13 +151,40 @@
                    v-btn(v-bind="props") Tooltip
            p we're going to need to revisit tooltip
 
-       div( class = "mt-4" )
+       div( class = "mt-2" )
            v-virtual-scroll(
                :height="200"
                :items="['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']"
                )
                template(v-slot:default="{ item }") Item {{ item }}
            p and virtual scroll
+
+       // TWELVE: skeleton loader
+       div( class = "my-5")
+           v-btn(
+               variant="outlined"
+               class = "mb-2"
+               block
+               color="cyan"
+               @click="loading=!loading"
+               ) toggle loading
+           // using if, there is also a way using slot
+           v-skeleton-loader(
+               v-if="loading"
+               type="card"
+               ) 
+           v-card(
+               v-if="!loading"
+               ) 
+               v-img(
+                   cover
+                   inline
+                   max-height="200"
+                   src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+                   ) 
+               v-divider
+               v-card-text here!
+           p ^skeleton loader
            
 </template>
 
@@ -127,6 +193,10 @@
        data: () => ({
            reveal: false,
            dialog: false,
+           loading: true,
+           snackbar: false,
+           snackbar_text: "Hello, I'm a snackbar"
+//           isActive: false
        }),
    }
 </script>
